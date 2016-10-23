6bf4e285ad342e42be10692519fc14b71e4ae689
NamTranDev <namtran09061992@gmail.com>
2016-10-23 22:00:47 +0700

    Card Page with viewpager

--- app/src/main/java/namtran/helperutil/ActivityExample/CardPageActivity.java
+++ app/src/main/java/namtran/helperutil/ActivityExample/CardPageActivity.java
@@ -2,12 +2,14 @@ package namtran.helperutil.ActivityExample;
 
 import android.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.view.View;
 import android.widget.ImageView;
 
 import namtran.helperutil.BaseActivity;
 import namtran.helperutil.R;
+import namtran.helperutil.mFragment.PageFragment;
 
 public class CardPageActivity extends BaseActivity {
 
@@ -32,6 +34,22 @@ public class CardPageActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         cardpage_img_background = (ImageView) findViewById(R.id.cardpage_img_background);
-        cardview_vp = (ViewPager) findViewById(R.id.cardview_vp);
+        cardview_vp = (ViewPager) findViewById(R.id.pager);
+
+        final FragmentStatePagerAdapter adapter = new FragmentStatePagerAdapter(getSupportFragmentManager()) {
+
+            @Override
+            public int getCount() {
+                return 20;
+            }
+
+            @Override
+            public android.support.v4.app.Fragment getItem(int position) {
+                return PageFragment.create(position);
+            }
+        };
+        cardview_vp.setAdapter(adapter);
+        cardview_vp.setClipToPadding(false);
+        cardview_vp.setPageMargin(20);
     }
 }
---
+++ app/src/main/java/namtran/helperutil/mFragment/PageFragment.java
@@ -0,0 +1,74 @@
+/*
+ * Copyright (C) 2014 Pixplicity
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package namtran.helperutil.mFragment;
+
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import namtran.helperutil.R;
+
+public class PageFragment extends Fragment {
+
+    private static final String ARG_PAGE_NUMBER = "pageNumber";
+
+    public static PageFragment create(int pageNumber) {
+        PageFragment fragment = new PageFragment();
+        Bundle bundle = new Bundle();
+        bundle.putInt(ARG_PAGE_NUMBER, pageNumber);
+        fragment.setArguments(bundle);
+        return fragment;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_page, container, false);
+        Button btItem = (Button) rootView.findViewById(R.id.bt_item);
+        final Bundle arguments = getArguments();
+        if (arguments != null) {
+            btItem.setText(
+                    getString(R.string.page_number_1d,
+                            arguments.getInt(ARG_PAGE_NUMBER) + 1));
+            btItem.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    new AlertDialog.Builder(getActivity())
+                            .setMessage(getString(R.string.page_number_1d,
+                                    arguments.getInt(ARG_PAGE_NUMBER) + 1))
+                            .setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    dialog.dismiss();
+                                }
+                            })
+                            .show();
+                }
+            });
+        } else {
+            btItem.setVisibility(View.GONE);
+        }
+        return rootView;
+    }
+
+}
