903d756b8c58ab2eb4bfe4f692465a2b1ea63b71
nam.t <namtran09061992@gmail.com>
2016-10-25 17:48:13 +0700

    card page with interface onpagechangelistener

--- app/src/main/java/namtran/helperutil/ActivityExample/CardPageActivity.java
+++ app/src/main/java/namtran/helperutil/ActivityExample/CardPageActivity.java
@@ -4,8 +4,10 @@ import android.app.Fragment;
 import android.os.Bundle;
 import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v4.view.ViewPager;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import namtran.helperutil.BaseActivity;
 import namtran.helperutil.R;
@@ -15,6 +17,20 @@ public class CardPageActivity extends BaseActivity {
 
     private ImageView cardpage_img_background;
     private ViewPager cardview_vp;
+    private static final float thresholdOffset = 0.5f;
+    private boolean scrollStarted, checkDirection;
+    private int positionImage;
+    private boolean isLeftorRight;
+    private ViewPagerOnPageChangeListener page;
+    private ViewPagerOnPageChangeListener pageNext;
+    private ViewPagerOnPageChangeListener pagePrevios;
+
+    public interface ViewPagerOnPageChangeListener{
+        void onPageScrolled(float positionOffset, int positionOffsetPixels);
+        void onPageScrolled(boolean isNext, float positionOffset, int positionOffsetPixels);
+        void onPageSelected(int position);
+    }
+
     @Override
     protected Fragment initFragment() {
         return null;
@@ -51,5 +67,71 @@ public class CardPageActivity extends BaseActivity {
         cardview_vp.setAdapter(adapter);
         cardview_vp.setClipToPadding(false);
         cardview_vp.setPageMargin(20);
+        cardview_vp.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+                page = (ViewPagerOnPageChangeListener) adapter.getItem(position);
+                page.onPageScrolled(positionOffset,positionOffsetPixels);
+
+                /*Log.d("Value offset","positionOffset : " + positionOffset
+                        + " \n" + "positionOffsetPixels : " + positionOffsetPixels);
+                if (checkDirection) {
+
+                    page = (ViewPagerOnPageChangeListener) adapter.getItem(position);
+
+                    if (thresholdOffset > positionOffset) {
+                        Toast.makeText(CardPageActivity.this, "Swiped Right", Toast.LENGTH_SHORT).show();
+                        isLeftorRight = true;
+                        pageNext = (ViewPagerOnPageChangeListener) adapter.getItem(position + 1);
+                    } else {
+                        Toast.makeText(CardPageActivity.this, "Swiped Left", Toast.LENGTH_SHORT).show();
+                        isLeftorRight = false;
+                        pagePrevios = (ViewPagerOnPageChangeListener) adapter.getItem(position - 1);
+                    }
+                    checkDirection = false;
+                }
+
+                if (page != null)
+                    page.onPageScrolled(positionOffset,positionOffsetPixels);
+
+                if (isLeftorRight){
+
+                    if (pageNext != null)
+                        pageNext.onPageScrolled(true,positionOffset,positionOffsetPixels);
+
+                    Log.d("Position", "Right");
+
+                }else {
+
+                    if (pagePrevios != null)
+                        pagePrevios.onPageScrolled(false,positionOffset,positionOffsetPixels);
+
+                    Log.d("Position", "LEFT");
+
+                }*/
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                if (page != null)
+                    page.onPageSelected(position);
+                if (pageNext != null)
+                    pageNext.onPageSelected(position);
+                if (pagePrevios != null)
+                    pagePrevios.onPageSelected(position);
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+                if (!scrollStarted && state == ViewPager.SCROLL_STATE_DRAGGING) {
+                    scrollStarted = true;
+                    checkDirection = true;
+                }else {
+                    scrollStarted = false;
+                }
+            }
+        });
     }
 }
--- app/src/main/java/namtran/helperutil/mFragment/PageFragment.java
+++ app/src/main/java/namtran/helperutil/mFragment/PageFragment.java
@@ -21,16 +21,22 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AlertDialog;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import namtran.helperutil.ActivityExample.CardPageActivity;
 import namtran.helperutil.R;
 
-public class PageFragment extends Fragment {
+public class PageFragment extends Fragment implements CardPageActivity.ViewPagerOnPageChangeListener {
 
     private static final String ARG_PAGE_NUMBER = "pageNumber";
+    private TextView txtTest;
 
     public static PageFragment create(int pageNumber) {
         PageFragment fragment = new PageFragment();
@@ -44,31 +50,47 @@ public class PageFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_page, container, false);
-        Button btItem = (Button) rootView.findViewById(R.id.bt_item);
+        txtTest = (TextView) rootView.findViewById(R.id.txtTest);
         final Bundle arguments = getArguments();
         if (arguments != null) {
-            btItem.setText(
-                    getString(R.string.page_number_1d,
-                            arguments.getInt(ARG_PAGE_NUMBER) + 1));
-            btItem.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    new AlertDialog.Builder(getActivity())
-                            .setMessage(getString(R.string.page_number_1d,
-                                    arguments.getInt(ARG_PAGE_NUMBER) + 1))
-                            .setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
-                                @Override
-                                public void onClick(DialogInterface dialog, int which) {
-                                    dialog.dismiss();
-                                }
-                            })
-                            .show();
-                }
-            });
-        } else {
-            btItem.setVisibility(View.GONE);
+            txtTest.setText("There is position : " + (arguments.getInt(ARG_PAGE_NUMBER) + 1));
         }
         return rootView;
     }
 
+    @Override
+    public void onPageScrolled(float positionOffset, int positionOffsetPixels) {
+        Log.d("Interface","Position Center");
+        if (txtTest != null){
+            Log.d("Interface","OK");
+        }else {
+            Log.d("Interface","Null");
+        }
+    }
+
+    @Override
+    public void onPageScrolled(boolean isNext, float positionOffset, int positionOffsetPixels) {
+        if (isNext){
+            Log.d("Interface","Position next");
+            if (txtTest != null){
+                txtTest.setText("There is position : " + positionOffset);
+            }else {
+                Log.d("Interface","Null");
+            }
+        }else {
+            Log.d("Interface","Position previous");
+            if (txtTest != null){
+                txtTest.setText("There is position : " + positionOffset);
+            }else {
+                Log.d("Interface","Null");
+            }
+        }
+    }
+
+    @Override
+    public void onPageSelected(int position) {
+        if (txtTest != null){
+            txtTest.setText("There is position : " + position);
+        }
+    }
 }
