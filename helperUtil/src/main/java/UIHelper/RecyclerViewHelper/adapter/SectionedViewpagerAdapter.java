4799e0f452887b4b35b4473fa7e267919963d83a
nam.t <namtran09061992@gmail.com>
2016-12-02 18:20:26 +0700

    add adapter helper for viewpager

--- helperUtil/src/main/java/UIHelper/RecyclerViewHelper/adapter/SectionedViewpagerAdapter.java
+++ helperUtil/src/main/java/UIHelper/RecyclerViewHelper/adapter/SectionedViewpagerAdapter.java
@@ -2,6 +2,7 @@ package UIHelper.RecyclerViewHelper.adapter;
 
 import android.content.Context;
 import android.support.v4.util.ArrayMap;
+import android.support.v4.view.PagerAdapter;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,15 +10,14 @@ import android.view.ViewGroup;
 /**
  * @author Aidan Follestad (afollestad)
  */
-public abstract class SectionedViewpagerAdapter extends MultiChoiceAdapter {
+public abstract class SectionedViewpagerAdapter extends PagerAdapter {
 
     private final static int VIEW_TYPE_HEADER = 0;
     private final static int VIEW_TYPE_ITEM = 1;
 
     private final ArrayMap<Integer, Integer> mHeaderLocationMap;
 
-    public SectionedViewpagerAdapter(Context context) {
-        super(context);
+    public SectionedViewpagerAdapter() {
         mHeaderLocationMap = new ArrayMap<>();
     }
 
@@ -25,13 +25,7 @@ public abstract class SectionedViewpagerAdapter extends MultiChoiceAdapter {
 
     public abstract int getItemCount(int section);
 
-    public abstract RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, boolean header);
-
-    public abstract void onBindHeaderViewHolder(RecyclerView.ViewHolder holder, int section);
-
-    public abstract void onBindViewHolder(RecyclerView.ViewHolder holder, int section, int position, int absolutePosition);
-
-    public abstract void onMultiChooseListener(View view, boolean state);
+    public abstract Object instantiateItem(ViewGroup container, int section, int position,int absolutePosition);
 
     public final boolean isHeader(int position) {
         return mHeaderLocationMap.get(position) != null;
@@ -62,7 +56,7 @@ public abstract class SectionedViewpagerAdapter extends MultiChoiceAdapter {
     }
 
     @Override
-    public final int getItemCount() {
+    public int getCount() {
         int count = 0;
         mHeaderLocationMap.clear();
         for (int s = 0; s < getSectionCount(); s++) {
@@ -72,50 +66,13 @@ public abstract class SectionedViewpagerAdapter extends MultiChoiceAdapter {
         return count;
     }
 
-    /**
-     * @hide
-     * @deprecated
-     */
     @Override
-    @Deprecated
-    public final RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return onCreateViewHolder(parent, viewType == VIEW_TYPE_HEADER);
-    }
-
-    /**
-     * @hide
-     * @deprecated
-     */
-    @Deprecated
-    @Override
-    public final int getItemViewType(int position) {
+    public Object instantiateItem(ViewGroup container, int position) {
         if (isHeader(position)) {
-            return VIEW_TYPE_HEADER;
-        } else {
-            return VIEW_TYPE_ITEM;
+            return null;
+        }else {
+            return instantiateItem(container,getSection(position)
+                    ,getPosition(position),position - (getSection(position) + 1));
         }
-    }
-
-    /**
-     * @hide
-     * @deprecated
-     */
-    @Override
-    @Deprecated
-    public final void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
-        super.onBindViewHolder(holder,position);
-        if (isHeader(position)) {
-            onBindHeaderViewHolder(holder, mHeaderLocationMap.get(position));
-        } else {
-            onBindViewHolder(holder, getSection(position),
-                    // offset section view positions
-                    getPosition(position),
-                    position - (getSection(position) + 1));
-        }
-    }
-
-    @Override
-    public void setActive(View view, boolean state) {
-        onMultiChooseListener(view,state);
     }
 }
